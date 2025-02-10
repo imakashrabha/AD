3b34726369c97f51de285cc580f2abb93b0d737e
imakashrabha <165803560+imakashrabha@users.noreply.github.com>
2025-02-10 10:39:27 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>𝖠𝖻𝗈𝗎𝗍...\n\n›› 𝖬𝖺𝖽𝖾 𝖿𝗈𝗋 : @ZoroFlix\n›› 𝖮𝗐𝗇𝖾𝖽 𝖻𝗒 : @imOnlyZoro !! \n</b>",
+            text = f"<b>𝖠𝖻𝗈𝗎𝗍...\n\n›› 𝖬𝖺𝖽𝖾 𝖿𝗈𝗋 : @ZoroFlix\n›› 𝖮𝗐𝗇𝖾𝖽 𝖻𝗒 : @imakashrabha !! \n</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup([
                 [InlineKeyboardButton("ʜᴏᴍᴇ", callback_data = "home"),
